27ee1edf4232cd13c4fe048f026b0e633761d933
edobbin <ethandobbin37@gmail.com>
2024-06-20 16:35:36 -0400

    Style change

--- Script.js
+++ Script.js
@@ -6,36 +6,54 @@
 //     return data.name;
 
 // }
-
-async function getGHAccount(username){
-    const url = `https://api.github.com/users/${username}`;
-    const response = await fetch(url);
-    const data = await response.json();
-    return {
-        user: data.login,
-        name: data.name,
-        id: data.id,
-        pic: data.avatar_url
-    };
-
-}
-
-getGHAccount("edobbin").then(info => console.log(info.pic));
-
+var card = document.getElementById("pfc");
 var pfp = document.getElementById("Profile-Picture");
 var textBox = document.getElementById("inpt");
 var button = document.getElementById("butt");
 var usernam = document.getElementById("user");
 
+var follow = document.getElementById("followers");
+var following = document.getElementById("following");
+var repo = document.getElementById("repo")
+
+async function getGHAccount(username){
+    const url = `https://api.github.com/users/${username}`;
+    const response = await fetch(url);
+    if(!response.ok ){
+        window.alert("Error");
+        card.style.visibility = 'hidden';
+        return;
+    }else{
+    const data = await response.json();
+    
+
+      return {
+        user: data.login,
+        name: data.name,
+        id: data.id,
+        pic: data.avatar_url,
+        follow: data.followers,
+        following: data.following,
+        repo: data.public_repos
+    };  
+    
+}
+
+}
+
+getGHAccount("edobbin").then(info => console.log(info.pic));
+
+
+
 button.addEventListener('click', () =>{
     const inputname = textBox.value;
     getGHAccount(inputname).then(
-        info => {pfp.src =info.pic; usernam.innerHTML = info.name}
+        info => {pfp.src =info.pic; usernam.innerHTML = info.name; follow.innerText = `${follow.innerText} ${info.follow}`; following.innerText = `${following.innerText} ${info.following}`; repo.innerText = `${repo.innerText} ${info.repo}`}
     )
 
     
 
-    
+    card.style.visibility = 'visible';
     console.log("click");
 });
 
